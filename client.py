3c61b4f4d4c63461d0dacc34865251c6651a9ce5
marskang <1124582842@qq.com>
2018-05-27 23:44:47 +0800

    add protobuf import

--- client.py
+++ client.py
@@ -1,17 +1,43 @@
 # -*- coding: utf-8 -*-
 
 import socket
-import person_pb2
+import message_pb2
+import player_pb2
+import struct
 
 HOST = '127.0.0.1'
 PORT = 8888
+LENGTH_HEADER = '!I'
+
+HEADER_LENGTH = struct.calcsize(LENGTH_HEADER)
+
+def recv_fill(sock, packet_len):
+    buff = ''
+    while len(buff) < packet_len:
+        data = sock.recv(packet_len - len(buff))
+        if not data:
+            return
+        buff += data
+    return buff
+
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-data = s.recv(1024)
-target = person_pb2.person()
-target.ParseFromString(data)
-print(target.id)
-print(target.name)
+
+_len = recv_fill(s, HEADER_LENGTH)
+(packet_len,) = struct.unpack(LENGTH_HEADER, _len)
+data = recv_fill(s, packet_len)
+'''
+per = person_pb2.person()
+per.ParseFromString(data)
+print per.id
+print per.name
+'''
+msg = message_pb2.Message()
+msg.ParseFromString(data)
+print msg.content
+print msg.player.id
+print msg.player.name
 s.close()
+
 
--- server.py
+++ server.py
@@ -1,35 +1,28 @@
 # -*- coding: utf-8 -*-
 
 from gevent.server import StreamServer
-import person_pb2
+import struct
+import message_pb2
+import player_pb2
 
-sockets = list()
+LENGTH_HEADER = '!I'
 
-def echo(_socket, address):
-    # print('New connection from %s:%s' % address)
-    #sockets.append(_socket)
-    #sockname =  _socket.getpeername()
-    #ip = sockname[0]
-    #port = sockname[1]
-    #while True:
-        #buff = _socket.recv(1024)
-        #for s in sockets:
-        #    peer_ip = s.getpeername()[0]
-        #    peer_port = s.getpeername()[1]
-        #    print peer_ip, peer_port, ip, port
-    per = person_pb2.person()
-    per.id = 6
-    per.name = 'zjk'
-    data = per.SerializeToString()
-    _socket.send(data)
-            
-            # if peer_ip == ip and peer_port == port:
-            #    continue
-            #s.send(buff)
-            
+
+def recv(sock, addr):
+    msg = message_pb2.Message()
+    player = msg.player
+    player.id = 1
+    player.name = '张三'
+    msg.content = 'hello world'
+    data = msg.SerializeToString()
+    packet_len = struct.pack(LENGTH_HEADER, len(data))
+    sock.sendall(packet_len + data)          
 
 if __name__ == '__main__':
-    server = StreamServer(('0.0.0.0', 8888), echo)
+    server = StreamServer(('0.0.0.0', 8888), recv)
     server.serve_forever()
+
+
+
 
 
